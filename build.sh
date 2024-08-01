602ffec6bf41dddb37d02654e30ae4834818ce01
4v3ngR <avenger@rescueteam.com>
2024-08-01 19:06:01 +0800

    update to v1.111.0

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-TAG=v1.110.0
+TAG=v1.111.0
 mkdir -p dist/$TAG
 pkgbuild --version $TAG --root LaunchDaemons --identifier com.unofficial.immich.installer --scripts Scripts --install-location /Library/LaunchDaemons dist/$TAG/Unofficial\ Immich\ Installer.pkg
