8aa90e07b70379a61d6122d5bdce9749f9f5200b
zebrapurring <>
2024-11-29 15:52:29 +0100

    Change installation directory to /opt/immich

--- Scripts/config.sh
+++ Scripts/config.sh
@@ -2,9 +2,9 @@
 
 export TAG="v1.121.0"
 
-export IMMICH_INSTALL_DIR="/opt/services/immich"
+export IMMICH_INSTALL_DIR="/opt/immich"
 export IMMICH_APP_DIR="$IMMICH_INSTALL_DIR/app"
-export IMMICH_HOME_DIR="$IMMICH_INSTALL_DIR/home"
+export IMMICH_HOME_DIR="/opt/immich/home"
 
 export IMMICH_USER="immich"
 export IMMICH_GROUP="immich"
