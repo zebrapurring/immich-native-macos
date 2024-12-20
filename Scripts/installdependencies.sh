186e3b87f072452923e4656904fad0c206f3fcf8
zebrapurring <>
2024-12-20 14:50:28 +0100

    Install ffmpeg from Jellyfin fork

--- Scripts/installdependencies.sh
+++ Scripts/installdependencies.sh
@@ -10,10 +10,11 @@ fi
 
 echo "INFO:  install dependencies"
 
-[ -z "$(which brew)" ] && echo "Homebrew is not installed" && exit 1
+mkdir -p "$IMMICH_BIN_DIR"
 
+# Install Homebrew dependencies
+[ -z "$(which brew)" ] && echo "Homebrew is not installed" && exit 1
 brew install \
-  ffmpeg \
   node \
   npm \
   pgvector \
@@ -24,3 +25,10 @@ brew install \
   wget
 brew services restart postgresql@17
 brew services restart redis
+
+# Install custom ffmpeg build jellyfin-ffmpeg
+curl -s -L -o - "https://github.com/jellyfin/jellyfin-ffmpeg/releases/download/v$IMMICH_FFMPEG_VERSION/jellyfin-ffmpeg_${IMMICH_FFMPEG_VERSION}_portable_mac64-gpl.tar.xz" | \
+  tar xzvf - -C "$IMMICH_BIN_DIR"
+
+# Adjust permissions
+chown -R "$IMMICH_USER:$IMMICH_GROUP" "$IMMICH_BIN_DIR"
